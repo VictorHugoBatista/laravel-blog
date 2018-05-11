b38a1ed57e63987e5e4b85c6144edc1eebf237c0
Victor Hugo <vct.bta@gmail.com>
2018-05-10 22:48:48 -0300

    Relaciona posts e comentários

--- app/Post.php
+++ app/Post.php
@@ -15,6 +15,11 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function tags()
     {
         return $this->belongsToMany(Tag::class);
